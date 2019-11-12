31fc02b16ac0554570b9907d03832a16ca96aac2
nolan <nolan796@live.ca>
2019-11-11 19:08:01 -0700

    Add ajax.get request on line 32, invoked on line 56, to render tweets upon page load

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,97 +1,59 @@
-const renderTweets = function(tweets) {
-  for (let tweet of tweets) {
-    let tweetString = createTweetElement(tweet)
-    $("#tweetBox").append(tweetString);
-  }
-};
-
-  // Bounce arrow slide composeNew (Don't change these values)
-$("#bounceArrow").on('click', function () {
-  $(".hideOnClick").slideToggle("slow", function() { 
-  $("#text-field").focus();
-  })
-});
-  
-  // Date and Time Function (Don't change these values)
-let timeAgo = function(date) {
-  if (typeof date !== 'object') {
-    date = new Date(date);
-  }
-  
-  let seconds = Math.floor((new Date() - date) / 1000);
-  let intervalType;
-
-  let interval = Math.floor(seconds / 31536000);
-  if (interval >= 1) {
-    intervalType = 'year';
-  } else {
-    interval = Math.floor(seconds / 2592000);
-    if (interval >= 1) {
-      intervalType = 'month';
-    } else {
-      interval = Math.floor(seconds / 86400);
-      if (interval >= 1) {
-        intervalType = 'day';
-      } else {
-        interval = Math.floor(seconds / 3600);
-        if (interval >= 1) {
-          intervalType = "hour";
-        } else {
-          interval = Math.floor(seconds / 60);
-          if (interval === 0) {
-            intervalType = 'a moment';
-          } else if (interval >= 1) {
-            intervalType = "minute";
-          } else {
-            interval = seconds;
-            intervalType = "second";
-          }
-        }
-      }
-    }
-  }
-
-  if (interval === 0) {
-    return intervalType;
-  } else {
-    intervalType += 's';
-    return interval + ' ' + intervalType;
-  }
-};
-  
 const createTweetElement = function(tweet) { 
-  
-  let newTime = timeAgo(new Date(tweet.created_at));
-  
+
   const escape =  function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   }
               // Dynamic Article Generator (Do not change these values)
-  const tweetString = `<article class=${"tweetContainer"}>           
-    <div>
-      <img src=${tweet.user.avatars}>${tweet.user.name}
-      </div>
-      <div class="handle">
-        <a>${tweet.user.handle}</a>
-      </div>
-      <p>${escape(tweet.content.text)}</p>        
-      <footer>
-        <h5>${newTime} ago 
-          <div class="articleIcons">
-            <i class="fas fa-heart"></i>
-            <i class="fas fa-flag"></i>
-            <i class="fas fa-retweet"></i>
-          </div>
-        </h5>
-      </footer>
-    </article>`;
+  const tweetString = `
+    <article class=${"tweetContainer"}>           
+      <div>
+        <img src=${tweet.user.avatars}>${tweet.user.name}
+       </div>
+       <div class="handle">
+         <a>${tweet.user.handle}</a>
+       </div>
+        <p>${escape(tweet.content.text)}</p>        
+       <footer>
+         <h5>${getTheCurrentTime(tweet.created_at)}  
+            <div class="articleIcons">
+             <i class="fas fa-heart"></i>
+              <i class="fas fa-flag"></i>
+             <i class="fas fa-retweet"></i>
+           </div>
+          </h5>
+       </footer>
+     </article>`;
   return tweetString;
 };
 
-$( document ).ready(function() {
+ // Load the tweets
+  const loadTweets = function() {
+    event.preventDefault();
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+      dataType: "json"
+    })
+      .done(function(data) {
+        $("#tweetBox").empty();
+        renderTweets(data);
+      })
+      .fail(function() {
+        console.log("error");
+      });
+  };
 
+const renderTweets = function(tweets) {
+  for (let tweet of tweets) {
+    let tweetString = createTweetElement(tweet)
+    $("#tweetBox").prepend(tweetString);
+  }
+};
+
+$( document ).ready(function() {
+  loadTweets();
   $(function() {  
     const $form = $('#newTweet');
     $form.on('submit', function (event) {
@@ -134,4 +96,25 @@ $( document ).ready(function() {
 });
 
 
+function getTheCurrentTime(date) {
+  var currentDate = Date.now();
+  var howLongAgoSeconds = (currentDate - date) / 1000 / 60;
+  var howLongAgoMinutes = (currentDate - date) / 1000 / 60;
+  var howLongAgoHours = (currentDate - date) / 1000 / 60 / 60;
+  if (howLongAgoMinutes < 1) {
+    return `a moment ago`;
+  } else if (howLongAgoMinutes > 1 && howLongAgoMinutes < 60) {
+    return `${Math.floor(howLongAgoMinutes)} minutes ago`;
+  } else if (howLongAgoMinutes > 60 && howLongAgoHours < 24) {
+    return `${Math.floor(howLongAgoHours)} hours ago`;
+  } else if (howLongAgoHours > 24) {
+    return `${Math.floor(howLongAgoHours / 24)} days ago`;
+  }
+}
 
+  // Bounce arrow slide composeNew (Don't change these values)
+  $("#bounceArrow").on('click', function () {
+    $(".hideOnClick").slideToggle("slow", function() { 
+    $("#text-field").focus();
+    })
+  });
